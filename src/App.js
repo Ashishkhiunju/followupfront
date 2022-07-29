dd2f07d764bf10856ac1f7c0489272904373606a
unknown <ashishkhinju123456789@gmail.com>
2022-07-29 15:58:33 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -47,9 +47,10 @@ import {UserEdit} from "./Components/User/UserEdit";
 import axios from 'axios';
 import "./App.css";
 
-axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "http://104.248.152.158:8000";
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
+
 axios.defaults.withCredentials = true;
 axios.interceptors.request.use(function(config){
   const token = localStorage.getItem('auth_token');
