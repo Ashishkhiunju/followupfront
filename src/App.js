75d59395abf8eab106ed988fd39981a381163ba9
unknown <ashishkhinju123456789@gmail.com>
2022-08-01 15:20:46 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ import axios from 'axios';
 import "./App.css";
 // http://127.0.0.1:8000/
 // http://sub.xinzatech.com/
-axios.defaults.baseURL = "http://sub.xinzatech.com/";
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
 
--- src/Components/Saving/SavingList.jsx
+++ src/Components/Saving/SavingList.jsx
@@ -1,82 +1,95 @@
-import React,{useEffect, useState} from 'react';
-import { MDBDataTable } from 'mdbreact';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+// import React,{useEffect, useState} from 'react';
+// import { MDBDataTable } from 'mdbreact';
+// import axios from 'axios';
+// import { Link } from 'react-router-dom';
+import DataTable from "./DataTable";
+import "../DataTable/DataTable.scss";
 
 export const SavingList= ()=>{
-    const [savings,setSaving]=useState([]);
-    useEffect(()=>{
-        fetchSavings();
-    },[])
+    // const [savings,setSaving]=useState([]);
+    // useEffect(()=>{
+    //     fetchSavings();
+    // },[])
 
-    const fetchSavings = ()=>{
-        axios.get('api/saving').then(({data})=>{
-            setSaving(data);
-        })
-    }
+    // const fetchSavings = ()=>{
+    //     axios.get('api/saving').then(({data})=>{
+    //         setSaving(data);
+    //     })
+    // }
 
-    const rows = savings.map((item)=>{
-        return ({
-            'name':item.customer.name,
-            'saving_type':item.saving_type,
-            'saving_amount':item.saving_amount,
-            'action':<div class="row">
-                        <div class="col-md-2">
-                            <Link to={`/saving-detail/${item.id}`} className='fa fa-eye' title="View Saving Detail"></Link>
-                        </div>
-                        <div class="col-md-2">
-                            <Link to={`/saving-preserve/${item.id}`} className='fa fa-save' title="Preserve"></Link>
-                        </div>
-                        <div class="col-md-2">
-                            <Link to={`/saving-withdraw/${item.id}`} className='fa fa-money' title="Withdraw"></Link>
-                        </div>
-                    </div>
-        })
-    })
+    // const rows = savings.map((item)=>{
+    //     return ({
+    //         'name':item.customer.name,
+    //         'saving_type':item.saving_type,
+    //         'saving_amount':item.saving_amount,
+    //         'action':<div class="row">
+    //                     <div class="col-md-2">
+    //                         <Link to={`/saving-detail/${item.id}`} className='fa fa-eye' title="View Saving Detail"></Link>
+    //                     </div>
+    //                     <div class="col-md-2">
+    //                         <Link to={`/saving-preserve/${item.id}`} className='fa fa-save' title="Preserve"></Link>
+    //                     </div>
+    //                     <div class="col-md-2">
+    //                         <Link to={`/saving-withdraw/${item.id}`} className='fa fa-money' title="Withdraw"></Link>
+    //                     </div>
+    //                 </div>
+    //     })
+    // })
 
-    const data = {
+    // const data = {
 
-        columns: [
-          {
-            label: 'Name',
-            field: 'name',
-            sort: 'asc',
-            width: 150
-          },
-          {
-            label: 'Saving Type',
-            field: 'saving_type',
-            sort: 'asc',
-            width: 150
-          },
-          {
-            label: 'Saving Amount',
-            field: 'saving_amount',
-            sort: 'asc',
-            width: 150
-          },
-          {
-            label: 'Action',
-            field: 'action',
-            sort: 'asc',
-            width: 150
-          },
-
-
-        ],
-        rows:rows
+    //     columns: [
+    //       {
+    //         label: 'Name',
+    //         field: 'name',
+    //         sort: 'asc',
+    //         width: 150
+    //       },
+    //       {
+    //         label: 'Saving Type',
+    //         field: 'saving_type',
+    //         sort: 'asc',
+    //         width: 150
+    //       },
+    //       {
+    //         label: 'Saving Amount',
+    //         field: 'saving_amount',
+    //         sort: 'asc',
+    //         width: 150
+    //       },
+    //       {
+    //         label: 'Action',
+    //         field: 'action',
+    //         sort: 'asc',
+    //         width: 150
+    //       },
 
 
-      };
+    //     ],
+    //     rows:rows
 
-return(
-    <><MDBDataTable
-    striped
-    bordered
-    processing
-  //   medium
-//   paging={false}
-    data={data}
-  /></>
+
+    //   };
+
+// return(
+//     <><MDBDataTable
+//     striped
+//     bordered
+//     processing
+//   //   medium
+// //   paging={false}
+//     data={data}
+//   /></>
+// )
+return (
+    <div className="card">
+        {/* <div className="card-header">Users</div> */}
+        <div className="card-body">
+            <DataTable
+                fetchUrl="/api/all-saving"
+                columns={["name", "saving_type", "saving_amount","action"]}
+            ></DataTable>
+        </div>
+    </div>
 )
 }
